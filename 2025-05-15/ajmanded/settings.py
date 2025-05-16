beef109c1a2425217b53954f7f1a3ec5f4e04aaf
Aromalunni-U <aromalunniu07@gmail.com>
2025-05-16 17:23:32 +0530

    Completed scraping dubizzle plp and changed ajmanded template format

--- 2025-05-15/ajmanded/settings.py
+++ 2025-05-15/ajmanded/settings.py
@@ -17,3 +17,20 @@ MONGO_URI = "mongodb://localhost:27017/"
 DB_NAME = "eservices_ajmanded"
 PARSE_COLLECTION = "parser"
 CRAWLER_COLLECTION = "crawler"  
+
+
+file_name = "2025-05-15/ajmanded/ajmanded.csv"
+
+FILE_HEADERS = [
+    "real_estate_link",
+    "license_number",
+    "license_type",
+    "legal_form",
+    "arabic_trade_name",
+    "english_trade_name",
+    "license_start_date" ,
+    "license_expiry_date",
+    "activities",
+    "est_banning_status",
+    "area"
+]
