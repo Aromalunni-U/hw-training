1f43f4c17576b7b19e03ab0264b08458b49b537d
Aromalunni-U <aromalunniu07@gmail.com>
2025-05-21 17:55:52 +0530

    Completed scraping delhaize's PLP and almost completed PDP

--- 2025-05-20/delhaize/crawler_delhaize.py
+++ 2025-05-20/delhaize/crawler_delhaize.py
@@ -1,17 +1,70 @@
-from curl_cffi import requests
-from parsel import Selector
-from settings import BASE_URL, headers, cookies
+import json
+import requests
+import logging
+from settings import  CRAWLER_COLLECTION, MONGO_URI, DB_NAME
+from urllib.parse import urljoin, quote
+from pymongo import MongoClient
 
 
-response = requests.get(
-    BASE_URL,
-    headers=headers,
-    cookies=cookies,
-    impersonate="chrome124",
-    timeout=30
-)
-html_path = "2025-05-20/delhaize/delhaize.html"
 
-if response.status_code == 200:
-    with open(html_path, "w") as file:
-        file.write(response.text)
+class Crawler:
+    def __init__(self):
+        self.client = MongoClient(MONGO_URI)
+        self.db = self.client[DB_NAME]
+        self.collection = self.db[CRAWLER_COLLECTION]
+
+    def start(self):
+        page_no = 1
+        product_links =[]
+
+        while True:
+            variables = {
+            "lang": "en",
+            "searchQuery": ":relevance",
+            "sort": "relevance",
+            "category": "v2FRU",
+            "pageNumber": page_no,
+            "pageSize": 50,
+            "filterFlag": True,
+            "fields": "PRODUCT_TILE",
+            "plainChildCategories": True
+            }
+
+            extensions = {
+                    "persistedQuery": {
+                        "version": 1,
+                        "sha256Hash": "4a9b385fdead25bb28350f1968ff36b8c44cf2def250653c8c81cbd3ece02e18"
+                    }
+                }
+            variables_encoded = quote(json.dumps(variables))
+            extensions_encoded = quote(json.dumps(extensions))
+
+            API_URL = f"https://www.delhaize.be/api/v1/?operationName=GetCategoryProductSearch&variables={variables_encoded}&extensions={extensions_encoded}"
+
+
+            response = requests.get(API_URL)
+
+            if response.status_code == 200:
+                products = response.json().get("data", {}).get("categoryProductSearch", {}).get("products", [])
+                if not products:
+                    break
+
+                new_links = []
+                for product in products:
+                    if "url" in product:
+                        product_url = urljoin("https://www.delhaize.be", product["url"])
+                        if product_url not in product_links:
+                            new_links.append({"url": product_url})
+
+                logging.info(new_links)
+                if new_links:
+                    self.collection.insert_many(new_links)
+                page_no += 1
+                
+            else:
+                logging.error(response.status_code)
+                break
+
+
+crawler = Crawler()
+crawler.start()
---
+++ 2025-05-20/delhaize/parser_delhaize.py
@@ -0,0 +1,64 @@
+import requests
+import logging
+from pymongo import MongoClient
+from settings import MONGO_URI, CRAWLER_COLLECTION, DB_NAME, PARSE_COLLECTION, HEADERS, BASE_API_URL
+
+
+class Parser:
+    def __init__(self):
+        self.client = MongoClient(MONGO_URI)
+        self.db = self.client[DB_NAME]
+        self.crawler_collection = self.db[CRAWLER_COLLECTION]
+        self.parser_collection = self.db[PARSE_COLLECTION]
+
+    def start(self):
+        links = self.crawler_collection.find()
+        for link in links:
+            link = link.get("url")
+            product_code = link.split("/")[-1]
+            params = {
+                "operationName": "ProductDetails",
+                "variables": f'{{"productCode":"{product_code}","lang":"en"}}',
+                "extensions": '{"persistedQuery":{"version":1,"sha256Hash":"18006c9fd3796b52136051c77d1c05b451b18ce72e6fdb191570da1de6d2089e"}}'
+            }
+            response = requests.get(BASE_API_URL, params=params)
+            if response.status_code == 200:
+                self.parse_item(link,response)
+            else:
+                logging.error(response.status_code)
+
+    def parse_item(self, url, response):
+        product = response.json().get("data", {}).get("productDetails",{})
+        price_details = product.get("price",{})
+        images = product.get("images", [])
+
+        name = product.get("name","")
+        product_code =  product.get("code","")
+        currency = price_details["currencySymbol"]
+        price = price_details.get("unitPrice","")
+        price_per_unit =  price_details.get("supplementaryPriceLabel1")
+        description = product.get("description","")
+
+        images_url = [
+           "https://www.delhaize.be" + img["url"] 
+           for img in images
+           if img.get("format") == "product"
+        ]
+
+        item = {
+            "product_url":url,
+            "name":name,
+            "product_code":product_code,
+            "currency":currency,
+            "price":price,
+            "price_per_unit":price_per_unit,
+            "description":description,
+            "imges_url":images_url
+        }
+        logging.info(item)
+        self.parser_collection.insert_one(item)
+
+
+
+parser = Parser()
+parser.start()
