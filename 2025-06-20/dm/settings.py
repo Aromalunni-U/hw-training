20567ecba9dbe260d84dd87d43eab8f5560e0699
Aromalunni-U <aromalunniu07@gmail.com>
2025-06-23 16:53:54 +0530

    add new file and code

---
+++ 2025-06-20/dm/settings.py
@@ -0,0 +1,14 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%H:%M:%S'
+)
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9"
+}
+
+sitemap_url = "https://products.dm.de/productfeed/SI/sitemap.xml"
