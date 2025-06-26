0cf1ed74d6f22381e7b3f3cbfca8ab0b122ee674
Aromalunni-U <aromalunniu07@gmail.com>
2025-06-26 17:13:49 +0530

    updated dm sitemap code

--- 2025-06-20/dm/dm_sitemap.py
+++ 2025-06-20/dm/dm_sitemap.py
@@ -1,19 +1,11 @@
 import requests
-import xml.etree.ElementTree as ET
+from xml.dom.minidom import parseString
 import logging
 from settings import HEADERS, sitemap_url
 
+response = requests.get(sitemap_url, headers=HEADERS)
+dom = parseString(response.content)
 
-response = requests.get(url=sitemap_url, headers= HEADERS)
-
-root = ET.fromstring(response.content)
-
-
-namespace = {"ns":"http://www.sitemaps.org/schemas/sitemap/0.9"}
-
-for url_tag in root.findall("ns:url",namespaces=namespace):
-    loc = url_tag.find("ns:loc",namespaces=namespace)
-    pdp_url = loc.text
-    logging.info(pdp_url)
-    
-
+for loc in dom.getElementsByTagName("loc"):
+    url = loc.firstChild.data
+    logging.info(url)
