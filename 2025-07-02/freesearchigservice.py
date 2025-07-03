640ff3a6b1ddae4302e382250a519a5446edd182
Aromalunni-U <aromalunniu07@gmail.com>
2025-07-03 17:44:08 +0530

    freesearchifservice feasibility not completed

--- 2025-07-02/freesearchigservice.py
+++ 2025-07-02/freesearchigservice.py
@@ -5,36 +5,59 @@ from io import BytesIO
 import pytesseract
 import urllib3
 import logging
-from settings import HEADERS, villages
+from settings import HEADERS
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 url = "https://freesearchigrservice.maharashtra.gov.in/"
-
-year = "2023"
+year = "2025"
 district = "30"
-village_input = "a"
-property_no = "9"
-selected_area = villages.get(village_input)[1] 
+village_input = "p"
+property_no = "11"
 
-
-logging.info(f"Selected Village: {selected_area}")
-
-res = requests.get(url, headers=HEADERS, verify=False)
-sel = Selector(text=res.text)
+session = requests.Session()
+response = session.get(url, headers=HEADERS, verify=False)
+sel = Selector(text=response.text)
 
 viewstate = sel.xpath('//input[@name="__VIEWSTATE"]/@value').get()
 eventvalidation = sel.xpath('//input[@name="__EVENTVALIDATION"]/@value').get()
 viewstategen = sel.xpath('//input[@name="__VIEWSTATEGENERATOR"]/@value').get()
 
-# CAPTHCA
-captcha_rel_url = sel.xpath('//img[@id="imgCaptcha"]/@src').get()
-captcha_url = f"https://freesearchigrservice.maharashtra.gov.in/{captcha_rel_url}"
-captcha_img = requests.get(captcha_url, headers=HEADERS, verify=False)
-img = Image.open(BytesIO(captcha_img.content))
-captcha_text = pytesseract.image_to_string(img)
+payload_area = {
+    '__EVENTTARGET': 'txtAreaName',
+    '__EVENTARGUMENT': '',
+    '__LASTFOCUS': '',
+    '__VIEWSTATE': viewstate,
+    '__VIEWSTATEGENERATOR': viewstategen,
+    '__EVENTVALIDATION': eventvalidation,
+    'ddlFromYear': year,
+    'ddlDistrict': district,
+    'txtAreaName': village_input,
+    'txtAttributeValue': '',
+    'ddlareaname': '-----Select Area----',
+    'txtImg': ''
+}
 
-logging.info(f"CAPTCHA : {captcha_text}")
+res_area = session.post(url, headers=HEADERS, data=payload_area, verify=False)
+sel_area = Selector(text=res_area.text)
+
+viewstate = sel_area.xpath('//input[@name="__VIEWSTATE"]/@value').get()
+eventvalidation = sel_area.xpath('//input[@name="__EVENTVALIDATION"]/@value').get()
+viewstategen = sel_area.xpath('//input[@name="__VIEWSTATEGENERATOR"]/@value').get()
+
+village_options = sel_area.xpath('//select[@id="ddlareaname"]/option/text()').getall()
+valid_areas = [v.strip() for v in village_options if v.strip() and "Select" not in v and v.lower().startswith(village_input.lower())]
+selected_area = valid_areas[0]
+
+logging.info(f"Selected Village: {selected_area}")
+
+captcha_rel_url = sel_area.xpath('//img[@id="imgCaptcha"]/@src').get()
+captcha_url = f"{url}{captcha_rel_url}"
+captcha_img = session.get(captcha_url, headers=HEADERS, verify=False)
+img = Image.open(BytesIO(captcha_img.content))
+captcha_text = pytesseract.image_to_string(img).strip()
+
+logging.info(f"CAPTCHA: {captcha_text}")
 
 payload_search = {
     '__EVENTTARGET': 'btnSearch',
@@ -51,7 +74,5 @@ payload_search = {
     'txtImg': captcha_text,
 }
 
-response = requests.post(url, headers=HEADERS, data=payload_search, verify=False)
+response = session.post(url, headers=HEADERS, data=payload_search, verify=False)
 
-
-logging.info(response.status_code)
--- 2025-07-02/settings.py
+++ 2025-07-02/settings.py
@@ -8,29 +8,14 @@ logging.basicConfig(
 
 
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Connection': 'keep-alive',
-}
-
-villages = {
-    'a': ['Akse', 'Akurdi', 'Akurli', 'Ambivali', 'Andheri', 'Anik', 'Are', 'Asalpha'],
-    'b': ['Badhavan', 'Bandivali', 'Bandra', 'Bapanale', 'Belapur', 'Bhandup', 'Boli', 'Borala', 'Borivali', 'Bramhanvada'],
-    'c': ['Chakala', 'Chandivali', 'Charkop', 'Chembur', 'Chinchavali', 'Chincholi', 'Chinchwad', 'Chuing'],
-    'd': ['Dahisar', 'Danda', 'Daravali', 'Darave', 'Dehugaon', 'Dehurod-Cantonment', 'Devanar', 'Dindoshi', 'Divale'],
-    'e': ['Eksar', 'Erangal'],
-    'g': ['Ghatkopar', 'Gorai', 'Goregaon', 'Gundavali', 'Gundavali Gundavali', 'Gundgaon'],
-    'h': ['Hariyali'],
-    'i': ['Ismaliya'],
-    'j': ['Juhu'],
-    'k': ['Kandivali', 'Kanheri', 'Kanjur', 'Karave', 'Khari', 'Kinhai', 'Kirol', 'Kiwale', 'Klarebad', 'Kolekalyan', 'Kondivita', 'Kopari', 'Kukashet', 'Kurar', 'Kurla'],
-    'm': ['Madh', 'Magathane', 'Mahul', 'Majas', 'Malad', 'Malavani', 'Malinagar', 'Mamuradi', 'Manbudruk', 'Mandale', 'Mandaneshwar', 'Mandapeshwar', 'Mankhurd', 'Manori', 'Maravali', 'Marol', 'Maroshi', 'Marve', 'Mogara', 'Mohili', 'Mulgaon', 'Mulund'],
-    'n': ['Nagathane', 'Nahur', 'Nanole', 'Nerul', 'Nigadi'],
-    'o': ['Oshivara'],
-    'p': ['pahadi goregaon', 'Parajapur', 'Parighakhar', 'Pasapoli', 'Pavai', 'Pi.ena.pahadieksar', 'Poisar'],
-    'r': ['Rahatani/kalewadi', 'Ravet'],
-    's': ['Sahar', 'Sai', 'Saki', 'Sanapada', 'Santakrujh', 'Sarsole', 'Shahabaj', 'Shimpavali', 'Shiravane', 'Sonkhar'],
-    't': ['Tirandaj', 'Tulasi', 'Tungaona', 'Turbhe'],
-    'v': ['Vadhavali', 'Vadhavan', 'Valnai', 'Varsova', 'Vikroli', 'Vileparle', 'Viththalanagar', 'Vyaravali']
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Priority': 'u=0, i',
 }
