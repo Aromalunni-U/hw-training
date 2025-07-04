51ff45cca19c6eaace067bebff4b53c884bfda7e
Aromalunni-U <aromalunniu07@gmail.com>
2025-07-04 17:47:59 +0530

    feasibility not completed

--- 2025-07-02/freesearchigservice.py
+++ 2025-07-02/freesearchigservice.py
@@ -9,27 +9,36 @@ from settings import HEADERS
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-url = "https://freesearchigrservice.maharashtra.gov.in/"
+def extract_tokens(sel):
+    viewstate = sel.xpath('//input[@name="__VIEWSTATE"]/@value').get()
+    viewstategen = sel.xpath('//input[@name="__VIEWSTATEGENERATOR"]/@value').get()
+    eventvalidation = sel.xpath('//input[@name="__EVENTVALIDATION"]/@value').get()
+
+    return {
+        "__VIEWSTATE": viewstate,
+        "__VIEWSTATEGENERATOR": viewstategen,
+        "__EVENTVALIDATION": eventvalidation
+    }
+
+BASE_URL = "https://freesearchigrservice.maharashtra.gov.in/"
+
 year = "2025"
 district = "30"
 village_input = "p"
 property_no = "11"
 
 session = requests.Session()
-response = session.get(url, headers=HEADERS, verify=False)
+response = session.get(BASE_URL, headers=HEADERS, verify=False)
 sel = Selector(text=response.text)
 
-viewstate = sel.xpath('//input[@name="__VIEWSTATE"]/@value').get()
-eventvalidation = sel.xpath('//input[@name="__EVENTVALIDATION"]/@value').get()
-viewstategen = sel.xpath('//input[@name="__VIEWSTATEGENERATOR"]/@value').get()
+
+tokens = extract_tokens(sel)
 
 payload_area = {
     '__EVENTTARGET': 'txtAreaName',
     '__EVENTARGUMENT': '',
     '__LASTFOCUS': '',
-    '__VIEWSTATE': viewstate,
-    '__VIEWSTATEGENERATOR': viewstategen,
-    '__EVENTVALIDATION': eventvalidation,
+    **tokens,
     'ddlFromYear': year,
     'ddlDistrict': district,
     'txtAreaName': village_input,
@@ -38,41 +47,98 @@ payload_area = {
     'txtImg': ''
 }
 
-res_area = session.post(url, headers=HEADERS, data=payload_area, verify=False)
-sel_area = Selector(text=res_area.text)
+res_area = session.post(BASE_URL, headers=HEADERS, data=payload_area, verify=False)
+sel = Selector(text=res_area.text)
+tokens = extract_tokens(sel)
 
-viewstate = sel_area.xpath('//input[@name="__VIEWSTATE"]/@value').get()
-eventvalidation = sel_area.xpath('//input[@name="__EVENTVALIDATION"]/@value').get()
-viewstategen = sel_area.xpath('//input[@name="__VIEWSTATEGENERATOR"]/@value').get()
-
-village_options = sel_area.xpath('//select[@id="ddlareaname"]/option/text()').getall()
+village_options = sel.xpath('//select[@id="ddlareaname"]/option/text()').getall()
 valid_areas = [v.strip() for v in village_options if v.strip() and "Select" not in v and v.lower().startswith(village_input.lower())]
+
+
 selected_area = valid_areas[0]
+logging.info(f"Selected area: {selected_area}")
 
-logging.info(f"Selected Village: {selected_area}")
+payload_area_select = {
+    '__EVENTTARGET': 'ddlareaname',
+    '__EVENTARGUMENT': '',
+    '__LASTFOCUS': '',
+    **tokens,
+    'ddlFromYear': year,
+    'ddlDistrict': district,
+    'txtAreaName': village_input,
+    'ddlareaname': selected_area,
+    'txtImg': ''
+}
 
-captcha_rel_url = sel_area.xpath('//img[@id="imgCaptcha"]/@src').get()
-captcha_url = f"{url}{captcha_rel_url}"
+res_area_select = session.post(BASE_URL, headers=HEADERS, data=payload_area_select, verify=False)
+sel = Selector(text=res_area_select.text)
+tokens = extract_tokens(sel)
+
+captcha_url = sel.xpath('//img[@id="imgCaptcha"]/@src').get()
+captcha_url = f"{BASE_URL}{captcha_url}"
 captcha_img = session.get(captcha_url, headers=HEADERS, verify=False)
 img = Image.open(BytesIO(captcha_img.content))
 captcha_text = pytesseract.image_to_string(img).strip()
 
 logging.info(f"CAPTCHA: {captcha_text}")
 
+HEADERS.update({
+    "X-MicrosoftAjax": "Delta=true",
+    "X-Requested-With": "XMLHttpRequest"
+})
+
 payload_search = {
-    '__EVENTTARGET': 'btnSearch',
-    '__EVENTARGUMENT': '',
-    '__LASTFOCUS': '',
-    '__VIEWSTATE': viewstate,
-    '__VIEWSTATEGENERATOR': viewstategen,
-    '__EVENTVALIDATION': eventvalidation,
-    'ddlFromYear': year,
-    'ddlDistrict': district,
-    'txtAreaName': village_input,
-    'ddlareaname': selected_area,
-    'txtAttributeValue': property_no,
-    'txtImg': captcha_text,
+    "ScriptManager1": "UpMain|btnSearch",
+    "__EVENTTARGET": "",
+    "__EVENTARGUMENT": "",
+    "__LASTFOCUS": "",
+    **tokens,
+    "__ASYNCPOST": "true",
+    "ddlFromYear": year,
+    "ddlDistrict": district,
+    "txtAreaName": village_input,
+    "ddlareaname": selected_area,
+    "txtAttributeValue": property_no,
+    "txtImg": captcha_text,
+    "FS_PropertyNumber": "",
+    "FS_IGR_FLAG": "",
+    "btnSearch": "शोध / Search"
 }
 
-response = session.post(url, headers=HEADERS, data=payload_search, verify=False)
+response = session.post(BASE_URL, headers=HEADERS, data=payload_search, verify=False)
+sel = Selector(text = response.text)
+
+tokens = extract_tokens(sel)
+
+
+indexii_buttons = sel.xpath("//input[@value='IndexII']/@onclick").getall()
+logging.info(indexii_buttons)
+
+index = 'indexII$2'
+
+payload_index = {
+    "ScriptManager1": "upRegistrationGrid|RegistrationGrid",
+    "__EVENTTARGET": "RegistrationGrid",
+    "__EVENTARGUMENT": index,
+    "__LASTFOCUS": "",
+    **tokens,
+    "__ASYNCPOST": "true",
+    "ddlFromYear": year,
+    "ddlDistrict": district,
+    "txtAreaName": village_input,
+    "ddlareaname": selected_area,
+    "txtAttributeValue": property_no,
+    "txtImg": captcha_text,
+    "FS_PropertyNumber": "",
+    "FS_IGR_FLAG": ""
+    }
+
+res_index = session.post(BASE_URL, headers=HEADERS, data=payload_index, verify=False)
+
+url = "https://freesearchigrservice.maharashtra.gov.in/rptIndex2_regLive.aspx"
+
+response = session.get(url, headers=HEADERS, verify=False)
+
+
+
 
--- 2025-07-02/settings.py
+++ 2025-07-02/settings.py
@@ -6,16 +6,11 @@ logging.basicConfig(
     datefmt='%H:%M:%S'
 )
 
-
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Sec-Fetch-User': '?1',
-    'Priority': 'u=0, i',
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Referer": "https://freesearchigrservice.maharashtra.gov.in/",
+    "Content-Type": "application/x-www-form-urlencoded",
+     "Cache-Control": "no-cache"
 }
